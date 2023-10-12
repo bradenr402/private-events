cf52c3d6f5c54d2776c9108e2a087a15e2092dab
Braden <108199884+bradenr402@users.noreply.github.com>
2023-10-12 12:41:39 -0400

    Project complete

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,5 @@
 class EventsController < ApplicationController
-
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @events = Event.all
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => {
-    registrations: 'registrations'
-  }
+  root "events#index"
+
+  devise_for :users, :controllers => { registrations: 'registrations' }
 
   devise_scope :user do
     get 'sign_out', to: 'devise/sessions#destroy', as: 'sign_out_path'
     get 'sign_in', to: 'devise/sessions#new', as: 'sign_in_path'
   end
-
-  root "events#index"
 
   resources :events do
     member do
